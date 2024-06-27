7862ceaa074f74f3863781bc73188a81009ab37b
dimar1150 <158440383+Dimar1510@users.noreply.github.com>
2024-06-27 14:07:36 +0300

    implement like controller

---
+++ server/controllers/like-controller.js
@@ -0,0 +1,67 @@
+const { prisma } = require("../prisma/prisma-client");
+const createError = require("./createError");
+
+const LikeController = {
+  likePost: async (req, res) => {
+    const { postId } = req.body;
+    const userId = req.user.userId;
+
+    if (!postId) {
+      return res.status(400).json({ error: createError().fieldMissing() });
+    }
+
+    try {
+      const existingLike = await prisma.like.findFirst({
+        where: { postId, userId },
+      });
+      if (existingLike) {
+        return res
+          .status(400)
+          .json({ error: "This user already liked this post" });
+      }
+      const like = await prisma.like.create({
+        data: {
+          userId,
+          postId,
+        },
+      });
+      res.json(like);
+    } catch (error) {
+      console.error(createError().controller("likePost"), error);
+      res.status(500).json({ error: createError().internal() });
+    }
+  },
+
+  unlikePost: async (req, res) => {
+    const { id } = req.params;
+    const userId = req.user.userId;
+
+    try {
+      const post = await prisma.post.findUnique({
+        where: { id },
+      });
+
+      if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+      }
+
+      const like = await prisma.like.findFirst({
+        where: { postId: id, userId },
+      });
+
+      if (!like) {
+        return res.status(404).json({ error: "Like not found" });
+      }
+
+      const deleteLike = await prisma.like.deleteMany({
+        where: { postId: id, userId },
+      });
+      res.json(deleteLike);
+    } catch (error) {
+      console.error(createError().controller("unlikePost"), error);
+      res.status(500).json({ error: createError().internal() });
+    }
+  },
+};
+
+module.exports = LikeController;
