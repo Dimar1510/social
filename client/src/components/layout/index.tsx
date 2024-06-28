37610b053bd4b90b99705881aef4dae4bf38b74c
dimar1150 <158440383+Dimar1510@users.noreply.github.com>
2024-06-28 18:09:43 +0300

    profile component

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -5,6 +5,7 @@ import Wrapper from "../wrapper"
 import { useSelector } from "react-redux"
 import { selectIsAuthenticated, selectUser } from "../../features/userSlice"
 import { useEffect } from "react"
+import Profile from "../profile"
 
 const Layout = () => {
   const isAuthenticated = useSelector(selectIsAuthenticated)
@@ -25,6 +26,9 @@ const Layout = () => {
         </div>
         <div className="flex-1 p-4">
           <Outlet />
+        </div>
+        <div className="flex-2 p-4">
+          <div className="flex flex-col gap-5">{!user && <Profile />}</div>
         </div>
       </Wrapper>
     </>
