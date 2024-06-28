97d38fb8a438644155fa3dfc34389c36360bc957
dimar1150 <158440383+Dimar1510@users.noreply.github.com>
2024-06-28 10:50:00 +0300

    theme provider

---
+++ client/src/components/theme-provider/index.tsx
@@ -0,0 +1,36 @@
+import { ReactNode, createContext, useState } from "react"
+
+type ThemeContextType = {
+  theme: "dark" | "light"
+  toggleTheme: () => void
+}
+
+export const ThemeContext = createContext<ThemeContextType>({
+  theme: "dark",
+  toggleTheme: () => null,
+})
+
+const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const storedTheme = localStorage.getItem("theme")
+  const currentTheme = storedTheme ? (storedTheme as "dark" | "light") : "dark"
+
+  const [theme, setTheme] = useState(currentTheme)
+
+  const toggleTheme = () => {
+    setTheme(prev => {
+      const newTheme = prev === "dark" ? "light" : "dark"
+      localStorage.setItem("theme", newTheme)
+
+      return newTheme
+    })
+  }
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <main className={`${theme} text-foreground bg-background`}>
+        {children}
+      </main>
+    </ThemeContext.Provider>
+  )
+}
+
+export default ThemeProvider
