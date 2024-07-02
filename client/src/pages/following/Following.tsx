ff60ff42f0e97dad820937f07904f03248057173
dimar1150 <158440383+Dimar1510@users.noreply.github.com>
2024-07-02 14:58:09 +0300

    search feature

--- client/src/pages/following/Following.tsx
+++ client/src/pages/following/Following.tsx
@@ -5,12 +5,14 @@ import { Card, CardBody } from "@nextui-org/react"
 import User from "../../components/user"
 import { useGetUserByIdQuery } from "../../app/services/userApi"
 import Back from "../../components/ui/back/Back"
+import SearchInput from "../../components/ui/searchInput/SearchInput"
+import { useState } from "react"
 
 const Following = () => {
   const params = useParams<{ id: string }>()
   const { data } = useGetUserByIdQuery(params?.id ?? "")
   const currentUser = useSelector(selectCurrent)
-
+  const [search, setSearch] = useState("")
   if (!data) {
     return <h2>User not found</h2>
   }
@@ -30,24 +32,31 @@ const Following = () => {
       </Card>
       {data.following.length > 0 ? (
         <div className="flex gap-5 flex-col">
+          <SearchInput value={search} onChange={setSearch} />
           <h2 className="text-xl text-center">
             {isCurrentUser ? `You are ` : `${data.name} is `}
             {`following (${data.followers.length}):`}
           </h2>
-          <div className="flex gap-4 flex-wrap justify-center overflow-hidden">
-            {data.following?.map(user => (
-              <Link to={`/users/${user.following.id}`} key={user.following.id}>
-                <Card shadow="sm">
-                  <CardBody className="block">
-                    <User
-                      name={user.following.name ?? ""}
-                      avatarUrl={user.following.avatarUrl ?? ""}
-                      description={user.following.email ?? ""}
-                    />
-                  </CardBody>
-                </Card>
-              </Link>
-            ))}
+          <div className="min-h-[40px] flex gap-4 flex-wrap justify-center overflow-hidden relative after:text-center after:text-lg after:content-['No_search_results'] after:size-full after:absolute after:left-0 after:top-0 after:z-50 after:hidden empty:after:block">
+            {data.following?.map(
+              user =>
+                user.following.name?.toLowerCase().includes(search) && (
+                  <Link
+                    to={`/users/${user.following.id}`}
+                    key={user.following.id}
+                  >
+                    <Card shadow="sm">
+                      <CardBody className="block">
+                        <User
+                          name={user.following.name ?? ""}
+                          avatarUrl={user.following.avatarUrl ?? ""}
+                          description={user.following.email ?? ""}
+                        />
+                      </CardBody>
+                    </Card>
+                  </Link>
+                ),
+            )}
           </div>
         </div>
       ) : (
