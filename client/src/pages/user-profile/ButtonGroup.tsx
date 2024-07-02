07c09721d1810e83cced9196e6755e249e1fe0cb
dimar1150 <158440383+Dimar1510@users.noreply.github.com>
2024-07-02 09:55:43 +0300

    component refactoring

---
+++ client/src/pages/user-profile/ButtonGroup.tsx
@@ -0,0 +1,65 @@
+import { Button } from "@nextui-org/react"
+import {
+  MdOutlinePersonAddAlt1,
+  MdOutlinePersonAddDisabled,
+} from "react-icons/md"
+import { CiEdit } from "react-icons/ci"
+import { User } from "../../app/types"
+
+type Props = {
+  data: User
+  handleFollow: () => void
+  isCurrentUser: boolean
+  onOpen: () => void
+}
+
+const ButtonGroup: React.FC<Props> = ({
+  data,
+  isCurrentUser,
+  handleFollow,
+  onOpen,
+}) => {
+  return (
+    <>
+      <div className="flex flex-col gap-4">
+        {!isCurrentUser ? (
+          <div className="group w-[120px]">
+            <Button
+              fullWidth
+              color={data.isFollowing ? "danger" : "primary"}
+              variant="flat"
+              className="gap-2 items-center hidden group-hover:flex"
+              onClick={handleFollow}
+              endContent={
+                data.isFollowing ? (
+                  <MdOutlinePersonAddDisabled />
+                ) : (
+                  <MdOutlinePersonAddAlt1 />
+                )
+              }
+            >
+              {data.isFollowing ? "Unfollow" : "Follow"}
+            </Button>
+            <Button fullWidth className="group-hover:hidden ">
+              {data.isFollowing ? "Following" : "Not following"}
+            </Button>
+          </div>
+        ) : (
+          <Button onClick={() => onOpen()} endContent={<CiEdit />}>
+            Edit profile
+          </Button>
+        )}
+        <Button>
+          <span>{data.following.length}</span>
+          <span>following</span>
+        </Button>
+        <Button>
+          <span>{data.followers.length}</span>
+          <span>follower{data.followers.length !== 1 && "s"}</span>
+        </Button>
+      </div>
+    </>
+  )
+}
+
+export default ButtonGroup
--- client/src/pages/user-profile/UserCard.tsx
+++ client/src/pages/user-profile/UserCard.tsx
@@ -1,5 +1,5 @@
 import { useLazyGetUserByIdQuery } from "../../app/services/userApi"
-import { Button, Card, CardBody, CardHeader, Image } from "@nextui-org/react"
+import { Card, CardBody, CardHeader, Image } from "@nextui-org/react"
 import { useSelector } from "react-redux"
 import { selectCurrent } from "../../features/userSlice"
 import {
@@ -9,17 +9,14 @@ import {
 
 import Back from "../../components/ui/back/Back"
 import { BASE_URL } from "../../constants"
-import {
-  MdOutlinePersonAddAlt1,
-  MdOutlinePersonAddDisabled,
-} from "react-icons/md"
-import { CiEdit, CiLocationOn } from "react-icons/ci"
+import { CiLocationOn } from "react-icons/ci"
 import { formatToClientDay } from "../../utils/format-to-client-day"
 import { FaBirthdayCake } from "react-icons/fa"
 import { BsCalendar3 } from "react-icons/bs"
 import { IoCheckmarkOutline } from "react-icons/io5"
 import { User } from "../../app/types"
 import ProfileItem from "./ProfileItem"
+import ButtonGroup from "./ButtonGroup"
 
 type Props = {
   data: User
@@ -53,7 +50,9 @@ const UserCard: React.FC<Props> = ({ data, userId, onOpen }) => {
       <Card className="flex-row p-2 mb-6" shadow="sm">
         <Back />
         <div className="flex flex-col">
-          <div className="font-bold break-words max-w-[300px]">{data.name}</div>
+          <div className="font-bold max-w-[300px] text-ellipsis break-words">
+            {data.name}
+          </div>
           <div className="text-small text-default-400 flex">
             {postsLength}
             {postsLength === 1 ? " post" : " posts"}
@@ -61,7 +60,7 @@ const UserCard: React.FC<Props> = ({ data, userId, onOpen }) => {
         </div>
       </Card>
       <Card className="p-3" shadow="sm">
-        <CardHeader className="flex justify-between items-start">
+        <CardHeader className="flex justify-between items-center xs:items-start flex-col xs:flex-row">
           <Image
             src={`${BASE_URL}${data.avatarUrl}`}
             alt={data.name}
@@ -69,49 +68,21 @@ const UserCard: React.FC<Props> = ({ data, userId, onOpen }) => {
             height={200}
             className="border-4 border-foreground-800 "
           />
-          <div className="flex flex-col items-end gap-4">
-            <div className="flex flex-col items-end">
-              <div className="text-2xl font-bold text-ellipsis break-words max-w-[300px]">
+          <div className="flex flex-col items-center xs:items-end gap-4">
+            <div className="flex flex-col items-center xs:items-end">
+              <div className="text-2xl font-bold text-ellipsis break-words max-w-[300px] text-center xs:text-right">
                 {data.name}
               </div>
-              <div className="text-lg ">{data.email}</div>
-            </div>
-
-            {!isCurrentUser ? (
-              <div className="group w-[120px]">
-                <Button
-                  fullWidth
-                  color={data.isFollowing ? "danger" : "primary"}
-                  variant="flat"
-                  className="gap-2 items-center hidden group-hover:flex"
-                  onClick={handleFollow}
-                  endContent={
-                    data.isFollowing ? (
-                      <MdOutlinePersonAddDisabled />
-                    ) : (
-                      <MdOutlinePersonAddAlt1 />
-                    )
-                  }
-                >
-                  {data.isFollowing ? "Unfollow" : "Follow"}
-                </Button>
-                <Button fullWidth className="group-hover:hidden ">
-                  {data.isFollowing ? "Following" : "Not following"}
-                </Button>
+              <div className="text-lg text-ellipsis break-words ">
+                {data.email}
               </div>
-            ) : (
-              <Button onClick={() => onOpen()} endContent={<CiEdit />}>
-                Edit profile
-              </Button>
-            )}
-            <Button>
-              <span>{data.following.length}</span>
-              <span>following</span>
-            </Button>
-            <Button>
-              <span>{data.followers.length}</span>
-              <span>follower{data.followers.length !== 1 && "s"}</span>
-            </Button>
+            </div>
+            <ButtonGroup
+              data={data}
+              handleFollow={handleFollow}
+              onOpen={onOpen}
+              isCurrentUser={isCurrentUser}
+            />
           </div>
         </CardHeader>
         <CardBody className="flex flex-col gap-2">
